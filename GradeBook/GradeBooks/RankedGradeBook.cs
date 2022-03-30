84e62576a083bd827806f28cd6a12e893e72f3ed
Adrian Klamerus <adrian.klamerus@microsoft.wsei.edu.pl>
2022-03-30 20:30:35 +0200

    Override RankedGradeBook's GetLetterGrade method & CalculateStudentStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -43,5 +43,28 @@ namespace GradeBook.GradeBooks
             }
             return 'F';
         }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
     }
 }
